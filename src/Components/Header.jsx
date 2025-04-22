f365250519dc32f347a4ace62d2a69e56e6764e8
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-22 13:31:41 +0300

    worked on header&footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,10 @@ import React from "react"
 
 export function Header() {
     return (
-        <div></div>
-    )
+      <div>
+        <header className="bg-blue-600 text-white p-4 shadow-md align-text-center">
+          <h1 className="text-xl font-semibold text-center">🛒 SHOPPING MANAGER 🛒</h1>
+        </header>
+      </div>
+    );
 }
