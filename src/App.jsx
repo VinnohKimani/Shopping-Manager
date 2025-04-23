74974c4dd8c27e9fa125f2c22eaf671fb8ba34f6
Johnson Alala <jonesalala95@gmail.com>
2025-04-23 16:51:58 +0300

    Add table section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "./Components/Header";
 import { ItemInput } from "./Components/ItemInput";
 import { ItemTable } from "./Components/ItemTable";
 import { Notification } from "./Components/Notification";
 import { Footer } from "./Components/Footer";
 
-
 function App() {
+  const [items, setItems] = useState([]);
+  const [budget, setBudget] = useState("");
+
   return (
     <div className="">
       <Header />
       <div className="grid grid-cols-12 gap-[10px] mx-2">
-      <ItemInput />
-      <ItemTable />
-      <Notification />
+        <ItemInput
+          items={items}
+          setItems={setItems}
+          budget={budget}
+          setBudget={setBudget}
+        />
+        <ItemTable items={items} setItems={setItems} budget={budget} />
+        <Notification />
       </div>
       <Footer />
     </div>
