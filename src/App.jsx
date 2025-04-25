00ed3565adc5dcb53849d8db3bd558b5cc64ae8c
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-25 12:49:49 +0300

    persisted budget

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,18 @@ import Contacts from "./Components/Contacts";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [budget, setBudget] = useState("");
+  const [budget, setBudget] = useState(() => {
+  //  here i'm getting the budget from the local storage 
+    const stored = localStorage.getItem("budget");
+    // and also  this is a conditon that when no budget is added the retrieval falls into an empty string 
+    // thats why we have empty qoutes
+   return stored ? JSON.parse(stored) : "";
+ });
 
+  // here when a new budget is added it is updated into the local storage
   const handleAddBudget = (newBudget) => {
     setBudget(newBudget);
+    localStorage.setItem("budget", JSON.stringify(newBudget))
   };
 
   //function to receive data from child itemInput
@@ -33,6 +41,24 @@ function App() {
   useEffect(() => {
     handleFetch();
   }, []);
+  
+  // here we retrieve the budget from the localStorage and puts it into state
+  // the handle fetch fetches the item data
+  useEffect(() => {
+    const storedBudget = localStorage.getItem("budget");
+    if (storedBudget) {
+      setBudget(storedBudget);
+    }
+      handleFetch();
+  }, []);
+
+  useEffect(() => {
+    if (budget !== "") {
+      localStorage.setItem("budget", budget);
+    }
+  }, [budget]);// this useEffect runs every time the budget value changes and that's why are able to get the
+  // the good to buy to change color when the you are within budget and when you are not
+
 
   const handleDelete = (id) => {
     fetch(`http://localhost:3004/shoppingitems/${id}`, {
@@ -44,6 +70,9 @@ function App() {
       })
       .catch((error) => console.error("Error deleting item:", error));
   };
+
+ 
+
 
   return (
     <div className="bg-slate-50">
